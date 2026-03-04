6c54fff60381e2d88c85f6d94b3e828ceffbfd26
SP-MICOM <superfamicom1022@gmail.com>
2026-03-04 21:43:56 +0900

    (2026.3.4) 예외 처리

--- Program/Container.h
+++ Program/Container.h
@@ -4,6 +4,17 @@ template<typename T>
 
 class Container
 {
+protected:
 	int size;
-};
+	int index;
 
+	T* list;
+public:
+	Container(int allocateSize);
+
+	void Push(T data);
+
+	const int& Sum();
+
+	~Container();
+};
